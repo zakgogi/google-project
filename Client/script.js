cf57833ae8fcbe5352677dd0c0e95a6a56de4850
keki94 <quequisegarra@gmail.com>
2021-06-29 17:15:11 +0100

    appendResuls create the list

--- Client/script.js
+++ Client/script.js
@@ -1,8 +1,8 @@
 let button = document.getElementById("submitBtn");
 button.addEventListener('click', getResults);
 
-async function getResults(){
-    // e.preventDefault();
+async function getResults(e){
+    e.preventDefault();
     let input = document.getElementById("searchInput").value;
     let data = await fetch(`http://localhost:3000/${input}`);
     let dataJson = await data.json();
@@ -11,6 +11,20 @@ async function getResults(){
 }
 
 function appendResults(data){
-    document.getElementById("resultsSection");
-
+    let html= ''
+    if (data instanceof Array){
+        for(object of data){
+            for(item of object.results){
+            html += `<a href= \'resultspage.html?input=${object.input}&result=${item}\'> ${object.input} Result content ${item} </a><br>`
+            }   
+        }
+    }
+    else {
+        for(item of data.results){
+            html += `<a href= \'resultspage.html?input=${data.input}&result=${item}\'> ${data.input} Result content ${item} </a><br>`
+        }
+    }
+    const section = document.getElementById("resultsSection");
+        
+    section.innerHTML = html;
 }
