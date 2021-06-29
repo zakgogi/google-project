7a1ac8c8bc8b3b64f747686a530504e2bf6abbf6
keki94 <quequisegarra@gmail.com>
2021-06-29 17:58:35 +0100

    trying to make the error work

--- Client/script.js
+++ Client/script.js
@@ -4,15 +4,21 @@ button.addEventListener('click', getResults);
 async function getResults(e){
     e.preventDefault();
     let input = document.getElementById("searchInput").value;
-    let data = await fetch(`http://localhost:3000/${input}`);
-    let dataJson = await data.json();
-    console.log(dataJson);
-    appendResults(dataJson);
+    try{
+        let data = await fetch(`http://localhost:3000/${input}`);
+        let dataJson = await data.json();
+        console.log(dataJson);
+        appendResults(dataJson);
+    } catch(e){
+        document.getElementById("resultsSection").innerHTML = '<p>Nothing for you :P</p>';
+        console.log(e.message);
+    }
+    
 }
 
 function appendResults(data){
     let html= ''
-    if (data instanceof Array){
+    if (data.length){
         for(object of data){
             for(item of object.results){
             html += `<a href= \'resultspage.html?input=${object.input}&result=${item}\'> ${object.input} Result content ${item} </a><br>`
@@ -20,9 +26,7 @@ function appendResults(data){
         }
     }
     else {
-        for(item of data.results){
-            html += `<a href= \'resultspage.html?input=${data.input}&result=${item}\'> ${data.input} Result content ${item} </a><br>`
-        }
+        
     }
     const section = document.getElementById("resultsSection");
         
