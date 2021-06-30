abf91a1d25fb5c351cdc7d90a9017e8ef05fddc1
keki94 <quequisegarra@gmail.com>
2021-06-30 11:45:29 +0100

    google :D

--- Client/script.js
+++ Client/script.js
@@ -1,3 +1,6 @@
+const KEY = "AIzaSyCaT_staXY_TnZHGGr987oqrgGMmVWNjDM";
+const CX = "9df169a68b2afe25c";
+
 let button = document.getElementById("submitBtn");
 button.addEventListener('click', getResults);
 
@@ -8,6 +11,7 @@ async function getResults(e){
         let data = await fetch(`http://localhost:3000/${input}`);
         let dataJson = await data.json();
         if(dataJson.message) throw new Error(dataJson.message);
+        getDataGoogle(input);
         appendResults(dataJson);
     } catch(e){
         document.getElementById("resultsSection").innerHTML = '<p>Nothing for you :P</p>';
@@ -15,6 +19,8 @@ async function getResults(e){
     }
     
 }
+
+
 
 function appendResults(data){
     let html= ''
@@ -24,8 +30,26 @@ function appendResults(data){
         html += `<a href= \'resultfinal.html?input=${object.input}&result=${item}\'> ${object.input} Result content ${item} </a><br>`
         }   
     }
+    
 
     const section = document.getElementById("resultsSection");
         
     section.innerHTML = html;
+}
+
+
+
+async function getDataGoogle(input){
+    try{
+        let data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${KEY}&cx=${CX}&q=${input}`);
+        console.log(data);
+        let dataJson = await data.json();
+        console.log(dataJson);
+        for(item of dataJson.items){
+            console.log(item);
+        }
+    }catch(e){
+        console.log(e);
+    }
+
 }
