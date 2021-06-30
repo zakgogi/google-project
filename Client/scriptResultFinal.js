79e1725562d494f21b69c9c4c7403c4b433c37fe
keki94 <quequisegarra@gmail.com>
2021-06-30 05:54:09 +0100

    sturdier against hack-attacks

--- Client/scriptResultFinal.js
+++ Client/scriptResultFinal.js
@@ -3,5 +3,5 @@ console.log(queryString);
 
 const urlParams = new URLSearchParams(queryString);
 
-document.getElementsByTagName('h1')[0].innerHTML = urlParams.get('input');
-document.getElementsByTagName('h2')[0].innerHTML = urlParams.get('result');
+document.getElementsByTagName('h1')[0].textContent = urlParams.get('input');
+document.getElementsByTagName('h2')[0].textContent = urlParams.get('result');
